25adb66d7c0489167065b8f27be4b6cbc30caf73
Zhiqian Zhao <99942146+zhiqan@users.noreply.github.com>
2023-02-17 20:20:49 +0800

    Update RCL.py

--- RCL.py
+++ RCL.py
@@ -10,7 +10,7 @@ import torch
 def RCNN(X_n): 
         N, C, W = X_n.size()
         p = np.random.rand()
-        K = [1, 3, 5, 7, 11, 15]
+        K = [1, 3, 5, 7, 11, 15]#Try adding random kernel sizes
         if p > 0.5:
             k = K[np.random.randint(0, len(K))]
             Conv = torch.nn.Conv1d(1, 1, kernel_size=k, stride=1, padding=k//2, bias=False)
