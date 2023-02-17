0f317a9f7574c26b952099883bc5d2fd020b3d72
Zhiqian Zhao <99942146+zhiqan@users.noreply.github.com>
2023-02-17 20:18:49 +0800

    Update RCL.py

--- RCL.py
+++ RCL.py
@@ -14,6 +14,6 @@ def RCNN(X_n):
         if p > 0.5:
             k = K[np.random.randint(0, len(K))]
             Conv = torch.nn.Conv1d(1, 1, kernel_size=k, stride=1, padding=k//2, bias=False)
-            torch.nn.init.xavier_normal_(Conv.weight)
+            torch.nn.init.kaiming_normal_(Conv.weight)
             X_n = Conv(X_n.reshape(-1, C, W)).reshape(N, C,  W)
         return X_n.detach()
